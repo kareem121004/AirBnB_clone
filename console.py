283cc622342b39c20b57f5d94f8a4d57f01cbc78
kareem121004 <karimadel11112004@gmail.com>
2023-12-09 14:38:43 -0800

    update

--- console.py
+++ console.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """Module for HBNBCommand class"""
-import models
+
 import cmd
-import json
+import re
+import models
 from models.base_model import BaseModel
 from models import storage
+import json
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
