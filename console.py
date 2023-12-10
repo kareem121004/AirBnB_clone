67e491135011a0fdd999a8f0778f43cca1ea4d54
kareem121004 <karimadel11112004@gmail.com>
2023-12-10 09:06:04 -0800

    hope

--- console.py
+++ console.py
@@ -1,33 +1,29 @@
 # !/usr/bin/python3
 
-    """An interactive shell?"""
+"""An interactive shell?"""
 
-    import cmd
-    import re
+import cmd
 
+class HBNBCommand(cmd.Cmd):
+    """Defines a class which is the entry point command interpreter"""
+    prompt = "(hbnb) "
+    def do_quit(self, line):
+        """Quit the interpreter"""
+        return True
 
-    class HBNBCommand(cmd.Cmd):
-        """Defines a class which is the entry point command interpreter"""
-        prompt = "(hbnb) "
+    def help_quit(self):
+        """help message for quit"""
+        print("Quit command to exit the program\n")
+        return
 
-        def do_quit(self, line):
-            """Quit the interpreter"""
-            return True
+    def do_EOF(self, line):
+        """Cleanly exits the"""
+        print()
+        return True
 
-        def help_quit(self):
-            """help message for quit"""
-            print("Quit command to exit the program\n")
-            return
+    def emptyline(self):
+        """Do nothing when empty line is entered"""
+        pass
 
-        def do_EOF(self, line):
-            """Cleanly exits the"""
-            print()
-            return True
-
-        def emptyline(self):
-            """Do nothing when empty line is entered"""
-            pass
-
-
-    if __name__ == '__main__':
-        HBNBCommand().cmdloop()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
