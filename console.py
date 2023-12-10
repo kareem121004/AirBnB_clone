a620b6d0841689ada237a33f71ed37c31a10786f
kareem121004 <karimadel11112004@gmail.com>
2023-12-10 08:43:22 -0800

    hell is

--- console.py
+++ console.py
@@ -3,27 +3,29 @@
 """An interactive shell?"""
 
 import cmd
+import re
 
 class HBNBCommand(cmd.Cmd):
-     prompt = '(hbnb)  '
-
-    def do_EOF(self, line):
-        """Exits console"""
-        print("")
-        return True
+    """Defines a class which is the entry point command interpreter"""
+    prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """Quit command to exit the program"""
-        print("Good Bye!")
+        """Quit the interpreter"""
         return True
 
     def help_quit(self):
-        """when two arguments involve"""
-        print('\n'.join(["Quit command to exit the program"]))
+        """help message for quit"""
+        print("Quit command to exit the program\n")
+        return
+
+    def do_EOF(self, line):
+        """Cleanly exits the"""
+        print()
+        return True
 
     def emptyline(self):
-        """ overwriting the emptyline method """
-        return False
+        """Do nothing when empty line is entered"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
