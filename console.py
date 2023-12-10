533cd45dc3db957725d4f7f1cfe6a521cff76c4b
kareem121004 <karimadel11112004@gmail.com>
2023-12-10 11:05:28 -0800

    update

--- console.py
+++ console.py
@@ -3,6 +3,16 @@
 """An interactive shell?"""
 
 import cmd
+import models
+from models.base_model import BaseModel
+from models import storage
+import json
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
